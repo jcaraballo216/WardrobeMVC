f4c9da3b299b64bb0a2d9e627f16f889a7cdfc8c
Jerome <jscar216@gmail.com>
2017-08-03 13:46:24 -0400

    Added database and Controllers

---
+++ WardrobeMVC/Controllers/ItemsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WardrobeMVC.Models;
+
+namespace WardrobeMVC.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private WardrobeEntities1 db = new WardrobeEntities1();
+
+        // GET: Items
+        public ActionResult Index()
+        {
+            var items = db.Items.Include(i => i.ClothingType).Include(i => i.Color).Include(i => i.Occassion).Include(i => i.Season);
+            return View(items.ToList());
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            ViewBag.TypeID = new SelectList(db.ClothingTypes, "TypeID", "TypeName");
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "ColorName");
+            ViewBag.OccassionID = new SelectList(db.Occassions, "OccassionID", "OccassionName");
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "SeasonName");
+            return View();
+        }
+
+        // POST: Items/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemID,Name,Photo,TypeID,ColorID,SeasonID,OccassionID")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TypeID = new SelectList(db.ClothingTypes, "TypeID", "TypeName", item.TypeID);
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "ColorName", item.ColorID);
+            ViewBag.OccassionID = new SelectList(db.Occassions, "OccassionID", "OccassionName", item.OccassionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "SeasonName", item.SeasonID);
+            return View(item);
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TypeID = new SelectList(db.ClothingTypes, "TypeID", "TypeName", item.TypeID);
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "ColorName", item.ColorID);
+            ViewBag.OccassionID = new SelectList(db.Occassions, "OccassionID", "OccassionName", item.OccassionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "SeasonName", item.SeasonID);
+            return View(item);
+        }
+
+        // POST: Items/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemID,Name,Photo,TypeID,ColorID,SeasonID,OccassionID")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TypeID = new SelectList(db.ClothingTypes, "TypeID", "TypeName", item.TypeID);
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "ColorName", item.ColorID);
+            ViewBag.OccassionID = new SelectList(db.Occassions, "OccassionID", "OccassionName", item.OccassionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "SeasonName", item.SeasonID);
+            return View(item);
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Item item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
